a38917067eade5d37fca5228111388e0b2098104
Quy Huy <huy.bq@neo-lab.vn>
2019-06-29 23:39:37 +0700

    ngchart-demo

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{Ngchart}}'));
+app.use(express.static('./dist/{{ngchart-demo}}'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{NgChart}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/{{ngchart-demo}}/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
