4088e7ae3c961ac425207be570f6c17d3b285d50
Quy Huy <huy.bq@neo-lab.vn>
2019-06-29 23:06:08 +0700

    heroku connect

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/Ngchart'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/{{Ngchart}}'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/Ngchart/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/{{NgChart}}/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
