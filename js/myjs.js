531d53efa18b912596e9f6f00332190ab8b6518b
Quy Huy <huy.bq@neo-lab.vn>
2019-06-29 22:34:48 +0700

    updates to deploy to heroku

--- js/myjs.js
+++
@@ -1,8 +0,0 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    nav: true,
-    navContainer: "#customNav",
-    dots: false,
-    margin: 20,
-    navText: ["<a class='fa fa-angle-left'>", "<a class='fa fa-angle-right'>"],
-})
